fea0813256eac547a2040bb642ffa0fac1cc20f6
luanhuynh009 <luanhuynh009@gmail.com>
2021-06-05 21:13:05 -0700

    Added JS functions to button

--- js/script.js
+++ js/script.js
@@ -8,7 +8,25 @@ const getImg = async function () {
         " https://picsum.photos/v2/list?limit=100"
     );
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
 }
 
-getImg();
+
+
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+}
+
+const displayImage = function(randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = `${author}`;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function(){
+    getImg();
+})
